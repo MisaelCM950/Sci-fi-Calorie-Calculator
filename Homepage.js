5f454885641e9e0b7e18225d4faa7980e862306e
Misa <misacm950@gmail.com>
2026-02-17 11:36:08 -0600

    Added unit converstion and sci-fi text

--- Homepage.js
+++ Homepage.js
@@ -1,12 +1,14 @@
 /*$$BMR = (10 \times \text{weight}) + (6.25 \times \text{height}) - (5 \times \text{age}) + \text{GenderOffset}$$*/
 
 function calculateCalories(){
-  const weight = parseFloat(document.getElementById("weight").value);
+  let weight = parseFloat(document.getElementById("weight").value);
   const height = parseFloat(document.getElementById("height").value);
   const age = parseFloat(document.getElementById("age").value);
   const activityMultiplier = parseFloat(document.getElementById("activity").value);
   const gender = document.querySelector('input[name="gender"]:checked').value;
 
+
+  const unit = document.getElementById("unit").value;
 
 
   if(isNaN(weight) || isNaN(height) || isNaN(age)){
@@ -14,6 +16,9 @@ function calculateCalories(){
     return;
   }
 
+  if(unit === "lbs"){
+    weight = weight / 2.20462
+  }
     let bmr = (10 * weight) + (6.25 * height) - (5 * age)
 
   if(gender === "male"){
